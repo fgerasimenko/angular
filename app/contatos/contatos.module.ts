f9e6949cdce81e66a004e2a945b4b6518ff4e982
Gerasimenko HC <fgerasimenko@hotmail.com>
2017-09-13 08:44:28 -0300

    Teste

---
+++ app/contatos/contatos.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContatosListaComponent } from './contatos-lista.component';
+import { ContatoRoutingModule } from './contato-routing.module';
+import { ContatoDetalheComponent } from './contato-detalhe.component';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ContatoRoutingModule
+        
+    ], 
+    declarations:[
+        ContatosListaComponent,
+        ContatoDetalheComponent
+    ],
+    exports: [
+        ContatosListaComponent
+    ]
+})
+
+export class ContatosModule{}
